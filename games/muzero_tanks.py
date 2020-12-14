6b9a34a3c967bb9ad35c7f7f05a3573e9241282f
Anton Maksimov <43805494+NeroxHSE@users.noreply.github.com>
2020-12-15 01:48:16 +0300

    Update muzero_tanks.py

--- games/muzero_tanks.py
+++ games/muzero_tanks.py
@@ -7,7 +7,7 @@ import torch
 #sys.path.insert(0, "muzero-general/Tanks_new")
 #sys.path.insert(0, "muzero-general/games")
 from .settings import Settings
-from .game_func import GameFunc
+from ..game_func import GameFunc
 from .abstract_game import AbstractGame
 
 
